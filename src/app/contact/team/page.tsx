1e47f6ba215cb7baa4f567ee9a2d7a35dcfde9a4
kacper0276 <kacper0276@op.pl>
2025-02-18 23:10:07 +0100

    generateMetadata

--- src/app/contact/team/page.tsx
+++ src/app/contact/team/page.tsx
@@ -1,4 +1,12 @@
 import { Menu } from "@/common/components/Menu";
+import { Metadata } from "next";
+
+export function generateMetadata(): Metadata {
+  return {
+    title: "Team",
+    description: "OPIS Team",
+  };
+}
 
 export default function TeamPage() {
   return (
