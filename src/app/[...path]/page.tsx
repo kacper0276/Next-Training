8d6f5dded4803c95f2c159196a730bab4131f9ea
kacper0276 <kacper0276@op.pl>
2026-02-10 23:15:28 +0100

    Add InfoContent type and update InfoPage to fetch and display content

--- src/app/[...path]/page.tsx
+++ src/app/[...path]/page.tsx
@@ -1,3 +1,5 @@
+import { fetchClient } from "@/common/clientApi/fetchClient";
+import { InfoContent } from "@/types/infoContent.type";
 import { NextPage } from "next";
 
 type InfoPageProps = {
@@ -6,10 +8,15 @@ type InfoPageProps = {
   };
 };
 
-const InfoPage: NextPage<InfoPageProps> = ({ params }) => {
+const InfoPage: NextPage<InfoPageProps> = async ({ params }) => {
+  const content = await fetchClient<InfoContent>(
+    `http://localhost:3004/contents/${encodeURIComponent(params.path.join("/"))}`,
+  );
+
   return (
     <div>
-      <h1>Info page</h1>
+      <h1>Info page {content.title}</h1>
+      <p>{content.body}</p>
       params {params.path.join("/")}
     </div>
   );
