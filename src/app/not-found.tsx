1e320b08dd04966ea0e9675620a4c18275721359
kacper0276 <kacper0276@op.pl>
2026-03-04 00:22:39 +0100

    Not found page

---
+++ src/app/not-found.tsx
@@ -0,0 +1,5 @@
+const NotFound = () => {
+  return <div>Custom page 404 - not found</div>;
+};
+
+export default NotFound;
---
+++ src/app/posts/[postId]/not-found.tsx
@@ -0,0 +1,5 @@
+const NotFound = () => {
+  return <div>Custom Post page 404 - not found</div>;
+};
+
+export default NotFound;
