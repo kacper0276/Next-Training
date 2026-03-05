e3620b65dd08a25144e2a150c68337d08ab01429
kacper0276 <kacper0276@op.pl>
2026-03-05 22:06:05 +0100

    Routing dynamiczny

--- pages-router/src/pages/index.tsx
+++ pages-router/src/pages/index.tsx
@@ -1,5 +1,14 @@
+import Link from "next/link";
+
 const HomePage = () => {
-  return <div>Home Page</div>;
+  return (
+    <div>
+      Home Page
+      <Link href="/contact">Go to Contact</Link>
+      <Link href="/contact/about-us">Go to About Us</Link>
+      <Link href="/posts">Go to Posts</Link>
+    </div>
+  );
 };
 
 export default HomePage;
---
+++ pages-router/src/pages/vlogs/[[...slug]].tsx
@@ -0,0 +1,5 @@
+const VlogPage = () => {
+  return <div>Vlog Page</div>;
+};
+
+export default VlogPage;
