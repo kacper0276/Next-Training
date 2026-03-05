fa7be6740bb246129b68c1a67f210f3b22a13a71
kacper0276 <kacper0276@op.pl>
2026-03-05 21:56:07 +0100

    Routing statyczny

---
+++ pages-router/src/pages/contact/about-us.tsx
@@ -0,0 +1,12 @@
+import Link from "next/link";
+
+const AboutUsPage = () => {
+  return (
+    <div>
+      <h1>About Us</h1>
+      <Link href="/contact">Back to Contact</Link>
+    </div>
+  );
+};
+
+export default AboutUsPage;
---
+++ pages-router/src/pages/contact/index.tsx
@@ -0,0 +1,5 @@
+const ContactPage = () => {
+  return <div>Contact Page in folder</div>;
+};
+
+export default ContactPage;
