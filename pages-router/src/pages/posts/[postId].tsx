fc72ee06e16aafea9d297a33a17449cd69ff74bb
kacper0276 <kacper0276@op.pl>
2026-03-11 23:37:13 +0100

    Implement server-side rendering for post details page

--- pages-router/src/pages/posts/[postId].tsx
+++ pages-router/src/pages/posts/[postId].tsx
@@ -1,5 +1,35 @@
-const PostPage = () => {
-  return <div>Post Page</div>;
+import { fetchClient } from "@/common/clientApi/fetchClient";
+import MainLayout from "@/common/components/layouts/MainLayout";
+import { Post } from "@/types/post.type";
+import { InferGetServerSidePropsType } from "next";
+
+type ServerSideProps = {
+  params: {
+    postId: string;
+  };
+};
+
+export const getServerSideProps = async ({ params }: ServerSideProps) => {
+  const post = await fetchClient<Post>(
+    `http://localhost:3004/posts/${params.postId}`,
+  );
+
+  return {
+    props: {
+      post,
+    },
+  };
+};
+
+const PostPage = ({
+  post,
+}: InferGetServerSidePropsType<typeof getServerSideProps>) => {
+  return (
+    <MainLayout>
+      <h1>{post.title}</h1>
+      <p>{post.body}</p>
+    </MainLayout>
+  );
 };
 
 export default PostPage;
