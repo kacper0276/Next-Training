d178e745a1cfdbe752b6a186f52156ad827d9511
kacper0276 <kacper0276@op.pl>
2026-03-12 00:12:20 +0100

    Add customProps to pageProps in MyApp component

--- pages-router/src/pages/_app.tsx
+++ pages-router/src/pages/_app.tsx
@@ -1,9 +1,11 @@
 import { AppProps } from "next/app";
 
 export default function MyApp({ Component, pageProps }: AppProps) {
+  const newProps = { ...pageProps, customProps: "my app prop" };
+
   return (
     <div>
-      This is _app.tsx <Component {...pageProps} />
+      This is _app.tsx <Component {...newProps} />
     </div>
   );
 }
