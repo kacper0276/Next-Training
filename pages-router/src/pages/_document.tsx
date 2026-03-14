b3b6617fbac9ea7efb57beaba9cf6c7e0793038e
kacper0276 <kacper0276@op.pl>
2026-03-14 08:29:34 +0100

    _document.tsx

---
+++ pages-router/src/pages/_document.tsx
@@ -0,0 +1,14 @@
+import { Head, Html, Main, NextScript } from "next/document";
+
+export default function Document() {
+  return (
+    <Html lang="pl">
+      <Head />
+      <body>
+        This is _document.tsx
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
+}
