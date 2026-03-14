015d357bae10884e53971d8d76c7a298cb25bd60
kacper0276 <kacper0276@op.pl>
2026-03-14 09:08:39 +0100

    Refactor font usage in layout and global styles to implement Roboto font

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,17 +1,7 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
 import { Menu } from "@/common/components/Menu/Menu";
 import "../styles/globals.scss";
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+import { roboto } from "@/styles/font";
 
 export const metadata: Metadata = {
   title: "Layout Next.js Title",
@@ -25,7 +15,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`${geistSans.variable} ${geistMono.variable}`}>
+      <body className={`${roboto.variable}`}>
         <Menu />
         <main className="main-container">{children}</main>
       </body>
